29e1e8030d02c93073c5b14e62d45b2dc8f21cd4
Charlie Carver <charlie@carv.is>
2018-04-14 10:54:53 -0400

    mta.sh now has access to the line and status for each subway

--- mta.sh
+++ mta.sh
@@ -1,2 +1,11 @@
-wget -qO- http://web.mta.info/status/serviceStatus.txt > status.txt
-
+wget -qO- http://web.mta.info/status/serviceStatus.txt > mta.txt
+cat mta.txt | tr "\n" "|" | grep -o "<subway>.*</subway>" | tr "|" "\n" > subways.txt
+cat subways.txt | grep "<line>" -A 2 | cut -d ">" -f2 | cut -d "<" -f1 | grep '\S' | grep -v "^--$" > lines_status.txt 
+i=1
+while [ $i -le `wc -l < lines_status.txt` ]
+do
+	line=`awk -v i=$i 'NR==i' < lines_status.txt`
+	status=`awk -v i=$((i + 1)) 'NR==i' < lines_status.txt`
+	echo "$line, $status"
+	i=$((i + 2))
+done
