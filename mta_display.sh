d136845d8e3bd4c673885258951fd73759d79d6c
Charlie Carver <charlie@carv.is>
2018-04-18 16:58:58 -0400

    It's basically done

--- mta_display.sh
+++ mta_display.sh
@@ -1,5 +1,8 @@
-trap 'tput setab 0; tput setaf 7; clear; stty echo sane; exit;' SIGINT SIGQUIT SIGTERM
+# it's a trap!
+trap 'rm s1.xml; rm s2.html; rm info.txt; rm s3.html; rm mta.txt; rm subways.txt; rm lines_status.txt; tput setab 0; tput setaf 7; clear; stty sane; exit;' SIGINT SIGQUIT SIGTERM
 
+# set the terminal mode to allow for non-blocking input
+# also don't want to echo any characters
 stty -echo -icanon time 0 min 0
 
 # color functions
@@ -16,14 +19,14 @@ reset() {
 	tput setab 0; tput setaf 7;
 }
 
-# how long to pause for, in centiseconds
+# how long to pause for between checks, in centiseconds because ???
 delay=500
 
 # box array dimensions
 num_box_rows=3
 num_box_cols=4
 
-# box spacing
+# box spacing and text spacing
 margin=2
 padding=2
 textPadding=2
@@ -47,6 +50,7 @@ start_position=$(($margin))
 
 # function to output the info
 output() {
+
 	# move cursor
 	tput cup $(($cursor_row + $textPadding * 1/2)) $(($cursor_col + $textPadding))
 
@@ -57,16 +61,20 @@ output() {
 	cursor_row=$(($cursor_row + 1))
 }
 
-# function to draw a colored background
+# compute the number of spaces needed per box/width only once
 spaces=""
 for c in `seq 0 $(($box_width - 1))`
 do
 	spaces+=" "
 done
+
+# function to draw a colored background
 draw_background() {
+	
 	# iterate through rows
 	for r in `seq 0 $(($box_height - 1))`; do
-		# iterate through columns
+	
+		# print each row with the predetermined number of spaces
 		tput cup $(($cursor_row + $r)) $(($cursor_col))
 		printf '%s' "$spaces"
 	done
@@ -74,6 +82,7 @@ draw_background() {
 
 # function to write the info in the box
 write_info() {
+
 	# set cursor position
 	cursor_row=$current_row
 	cursor_col=$current_col
@@ -97,95 +106,181 @@ write_info() {
 	# output all info
 	output "$line"
 	output "$status"
-	output "Press $fKey for more info"
+	output "Press \"${fKeyNames[$fKeyIndex]}\" for more info"
 
 	# reset background
 	reset
 }
 
-#function that checks for keypress
+# override flag will cancel out of the check_for_keypress loop when it equals 1
+# this happens after a line's status has been checked, triggering a clearing of the screen and refresh of data
 override=0
+
+# a list of function keys to view more info per train line
+# has more than enough values in case of future line additions
+fKeyNames=("1" "2" "3" "4" "5" "6" "7" "8" "9" "a" "b" "c")
+
+# function that checks for a keypress
 check_for_keypress() {
+
+	# reset the status html variable
 	status_html=""
+	
+	# read the input to the terminal
 	read input
-	if ! [[ -z $input ]]
+	if [[ "$input" == "q" ]]
 	then
-		line=${lines[$input]}
-		if ! [[ -z $line ]]
+		
+		# the user has decided to quit
+		rm s1.xml; rm s2.html; rm info.txt; rm s3.html; rm mta.txt; rm subways.txt; rm lines_status.txt;
+		tput setab 0; tput setaf 7; clear; stty sane; exit;		
+	else
+
+		# something else has been pressed
+		# loop through the saved function key names and see if one of them was pressed
+		line="-1"
+		j=0
+		for keyName in "${fKeyNames[@]}"
+		do
+			if [[ "$keyName" == "$input" ]]
+			then
+				
+				# a key has been pressed corresponding to a specific line
+				line=${lines[$j]}
+				break
+			fi
+			j=$((j + 1))
+		done
+		
+		# check if a line was registered
+		if [ "$line" != "-1" ]
 		then
-			cat subways.txt | grep $line -B 1 -A 10000 | sed -n '/<line>/,/<\/line>/p;/<\/line>/q' > s1.xml
+			
+			# parse the subways file for just the data pertaining to the specific line
+			cat subways.txt | grep "<name>$line</name>" -B 1 -A 100000 | sed -n '/<line>/,/<\/line>/p;/<\/line>/q' > s1.xml
+		
+			# parse the line for only the text information
 			cat s1.xml | sed -n '/<text>/,/<\/text>/p' | cut -d ">" -f2 | cut -d "<" -f1 > s2.html
-			echo Nothing to report! > info.txt
+			
+			# create the info file
+			echo "[LINE INFORMATION FOR $line]" > info.txt
+			echo >> info.txt
+			
+			# check if there is text information or an empty text tag, <text />
 			if [[ `cat s2.html | wc -l` -ge 1 ]]
 			then
-				#cat s2 | perl -MHTML::Entities -pe 'decode_entities($_);' > s3
+				
+				# the text tag was not empty, convert the html characters to html with lynx
 				lynx --dump s2.html > s3.html	
-				lynx --dump s3.html > info.txt
-				#echo `echo $status_html | perl -MHTML::Entities -pe 'decode_entities($_);'`
+
+				# now convert the html to text with lynx
+				lynx --dump s3.html >> info.txt
+			else
+				
+				# there was an empty text tag
+				echo "No information to report" >> info.txt
 			fi
+			
+			# output the data to a text file and display it with less
 			echo >> info.txt
-			echo [PRESS Q TO EXIT] >> info.txt
+			echo "[PRESS \"q\" TO QUIT]" >> info.txt
 			reset
 			clear
-			#echo "$(<s4.html)"
 			less info.txt
-			#read cancel
-			#while [[ -z $cancel ]]
-			#do
-			#	read cancel
-			#	sleep 0.01
-			#done
-			#clear
-			#reset
+			
+			# once less cancels, change the override flag to refresh the screen
 			override=1
 		fi
 	fi
 }
 
+# clear the screen initially
 clear
+
+# loop infinitely to display the terminal
 while true
 do
-	# reset coordinates
+
+	# reset coordinates and override flag
         current_row=$(($start_position + 2)) #reset + 2 to account for timestamp
         current_col=$start_position
 	override=0
 
+	# get the mta information from their "xml" (although it's not valid)
 	wget -qO- http://web.mta.info/status/serviceStatus.txt > mta.txt
+
+	# get the timestamp of the last updated data
 	timestamp=`cat mta.txt | grep timestamp | cut -d ">" -f5 | cut -d "<" -f1`
+
+	# parse out the non-subway related info
 	cat mta.txt | tr "\n" "|" | grep -o "<subway>.*</subway>" | tr "|" "\n" > subways.txt
+
+	# output a list of all the lines and their current statuses
 	cat subways.txt | grep "<line>" -A 2 | cut -d ">" -f2 | cut -d "<" -f1 | grep '\S' | grep -v "^--$" > lines_status.txt 
+
+	# reset fKeyIndex, correspoding to the function key label's index and the train line's index
+	fKeyIndex=0
 	
+	# reset i to 1 since awk indexing starts at 1
 	i=1
-	fKey=1
+	
+	# reset the delay timer
 	t=0
 	
+	# echo the last updated time
 	tput cup $(($margin)) $(($margin))
-	echo "Last updated: $timestamp"
+	echo "last updated: $timestamp"
 
+	# echo the quit message
+	quit="press \"q\" to quit"
+	tput cup $(($margin)) $(($window_width - ${#quit} - $margin))
+	echo $quit
+
+	# loop through each train line/status line
 	while [ $i -le `wc -l < lines_status.txt` ]
 	do
+		
+		# the train line resides on the (i)th line in the text file
 		line=`awk -v i=$i 'NR==i' < lines_status.txt`
+		
+		# the train status resides on the (i+1)th line in the text file
 		status=`awk -v i=$((i + 1)) 'NR==i' < lines_status.txt`
-		write_info
-		lines[fKey]=$line
 
+		# write the info to the display
+		write_info
+
+		# match the train line to the function key index
+		lines[fKeyIndex]=$line
+
+		# determine the current column and row
 		current_col=$(($current_col + $width_increment))
         	if ! (($current_col + $box_width < $window_width)); 
 		then 
 			current_col=$start_position
 	               	current_row=$(($current_row + $height_increment))
         	fi
-
+	
+		# increment the index read from the satus text file by 2, since each trainline is 2 text lines away from the next
 		i=$((i + 2))
-		fKey=$((fKey + 1))
+		
+		# increment the function key index
+		fKeyIndex=$((fKeyIndex + 1))
 	done
 
 	# Reset the cursor position
 	tput cup $window_height $window_width
+	
+	# check for a keypress at the end of the display
 	while [ $t -lt $delay ] && [ $override -eq 0 ]
 	do
+		
+		# do the checking
 		check_for_keypress
+
+		# increment time by 1
 		t=$((t + 1))
+
+		# this is off by a magnitude of 10 but it works so 
 		sleep 0.001
 	done
 	
